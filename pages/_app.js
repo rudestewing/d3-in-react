bb212ae91f086ea8c9e10bc0c0830b5729f20080
Rudi Setiawan <rudestewing@gmail.com>
2021-04-21 11:24:46 +0700

    Fix: Styling & fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import '../styles/font.scss'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
--- pages/bar.js
+++ pages/bar.js
@@ -3,6 +3,7 @@ import { Fragment } from 'react'
 import dynamic from 'next/dynamic'
 
 const Bar = dynamic(() => import('@/components/Bar'))
+const BarD3Only = dynamic(() => import('@/components/Bar/BarD3Only'))
 
 const PageComponent = () => {
   return (
@@ -13,6 +14,11 @@ const PageComponent = () => {
             <section className="content">
               <div className="container mx-auto">
                 <Bar />
+              </div>
+            </section>
+            <section>
+              <div className="container mx-auto">
+                <BarD3Only />
               </div>
             </section>
           </Fragment>
