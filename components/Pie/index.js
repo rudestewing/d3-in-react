19515a03b56de8f80c73140a216b90ba82232f66
Rudi Setiawan <rudestewing@gmail.com>
2021-04-22 05:02:06 +0700

    Feat: PieChart

---
+++ components/Pie/index.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react'
+import { scaleOrdinal, pie, arc, schemeCategory10, format } from 'd3'
+import {
+  width,
+  height,
+  innerWidth,
+  margin,
+  innerHeight,
+} from '@/lib/config/dimensions'
+
+const Pie = () => {
+  const [data, setData] = useState([])
+
+  function loadData() {
+    setTimeout(() => {
+      setData([
+        { label: 'Apples', value: 10 },
+        { label: 'Oranges', value: 20 },
+        { label: 'Kiwi', value: 42 },
+        { label: 'Pumpkin', value: 32 },
+      ])
+    })
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  const radius = Math.min(width, height) / 2 - 40
+
+  const createPie = pie().value((d) => d.value)
+
+  const createArc = arc().innerRadius(0).outerRadius(radius)
+
+  const dataReady = createPie(data)
+
+  const colorScale = scaleOrdinal(schemeCategory10)
+  const formatter = format('.2f')
+
+  return (
+    <svg width={width} height={height} style={{ margin: '0 auto' }}>
+      <g transform={`translate(${width / 2}, ${height / 2})`}>
+        {dataReady.map((d, index) => {
+          const [x, y] = createArc.centroid(d)
+
+          return (
+            <g key={index}>
+              <path d={createArc(d)} fill={colorScale(index)}></path>
+              <g>
+                <text
+                  textAnchor="middle"
+                  fontSize="0.9em"
+                  fill="white"
+                  transform={`translate(${x}, ${y - 10})`}
+                  // y="-10"
+                >
+                  {d.value}
+                </text>
+                <text
+                  textAnchor="middle"
+                  fontSize="0.9em"
+                  fill="white"
+                  transform={`translate(${x}, ${y + 10})`}
+                  // y="10"
+                >
+                  {d.data.label}
+                </text>
+              </g>
+            </g>
+          )
+        })}
+      </g>
+    </svg>
+  )
+}
+
+export default Pie
--- pages/pie.js
+++ pages/pie.js
@@ -1,5 +1,8 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import { Fragment } from 'react'
+import dynamic from 'next/dynamic'
+
+const Pie = dynamic(() => import('@/components/Pie'))
 
 const PageComponent = () => {
   return (
@@ -8,7 +11,14 @@ const PageComponent = () => {
         content: (
           <Fragment>
             <section className="content">
-              <div className="container mx-auto">Render pie chart here</div>
+              <div className="container mx-auto">
+                <h1 className="text-lg font-semibold text-center mb-5">
+                  Pie Chart Sample of data
+                </h1>
+                <div className="mb-4">
+                  <Pie />
+                </div>
+              </div>
             </section>
           </Fragment>
         ),
